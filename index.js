3875a3d5a24f2211c6e1bd1897b4dfac1d90bd40
Mahmoud Mansour <mahmodlte@gmail.com>
2020-06-13 07:37:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ class Cat {
 
 class Dog {
   constructor(name, sex){
-     this.name = name;
+    this.name = name;
     this.sex = sex;
   }
 }
 
 class Bird {
   constructor(name,sex){
-     this.name = name;
+    this.name = name;
     this.sex = sex;
   }
 }
